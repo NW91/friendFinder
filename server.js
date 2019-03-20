2d5d48e84a95a33c08d7ea3133c71207c016ed2b
Nicole W <nicoleewhitee@gmail.com>
2019-03-20 14:03:23 -0700

    skelton home page added

--- server.js
+++ server.js
@@ -3,18 +3,25 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
+//sets up server
 var PORT = process.env.PORT || 8080;
 
+//create application/json parser
 var jsonParser = bodyParser.json()
 
+//creates application form parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
+//parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
+//parse some custom thing into a buffer
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
+//parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+//function that reads HTMLRouting.js
 require("./app/routing/htmlRoutes") (app, path);
 
 
