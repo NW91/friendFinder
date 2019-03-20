bddff0502e7f3634503324dafab27fb28a84d2df
Nicole W <nicoleewhitee@gmail.com>
2019-03-20 11:06:31 -0700

    setting up routes

---
+++ app/data/friend.js
@@ -0,0 +1,28 @@
+var friendsData = [
+    {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
+  ];
+
+  var addFriend = function(app) {
+      app.post("/survey", function(req, res) {
+          var newFriend = req.body;
+          friendsData.push(newFriend);
+          res.send(friendsData);
+      });
+  };
+
+  module.exports = friendsData;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
@@ -13,6 +14,9 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/htmlRoutes") (app, path);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
