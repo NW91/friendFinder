16ff076ac4a742ad42269eae3dea298168cd0f41
Nicole W <nicoleewhitee@gmail.com>
2019-03-20 18:51:49 -0700

    newFriends array added

--- app/data/friend.js
+++ app/data/friend.js
@@ -14,7 +14,39 @@ var friendsData = [
         4,
         1
       ]
-    }
+    },
+    {
+    "name":"Ashley",
+    "photo":"https://depositphotos.com/117506590/stock-photo-nice-joyful-woman-eating-salad.html",
+    "scores":[
+            2,
+            4,
+            1,
+            5,
+            5,
+            1,
+            3,
+            3,
+            2,
+            1
+          ]
+    },
+    {
+        "name":"Robert",
+        "photo":"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiH4a6zj5LhAhVH_J4KHelzAT8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.istockphoto.com%2Fno%2Fphotos%2Fmen&psig=AOvVaw1Os6_1XSepuyyrif2YY_8p&ust=1553219425894955",
+        "scores":[
+            1,
+            2,
+            4,
+            4,
+            2,
+            5,
+            2,
+            5,
+            3,
+            5
+          ]
+        }
   ];
 
   var addFriend = function(app) {
