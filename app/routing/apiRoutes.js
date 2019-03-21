9c33476dd14ff9f0b1d8b953f8495de94ade100e
Nicole W <nicoleewhitee@gmail.com>
2019-03-21 14:57:09 -0700

    testing api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,42 @@
 var friendsData = require('../data/friend.js');
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        var perfectMatch = {
+            name: "",
+            photo: "",
+            friendDifference = 1000
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var totalDifference = 0;
+
+        //Loops through friendfinder in Database
+        for (var i = 0; i < friendsData.length; i++) {
+            totalDifference = 0;
+
+            //Looping through every score within the friendfinder Database.
+            for (var j = 0; j <friendsData[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+               
+            if (totalDifference <= perfectMatch.friendsDifference) {
+                perfectMatch.name = friendsData[i].name;
+                perfectMatch.photo = friendsData[i].photo;
+                perfectMatch.friendDifference = totalDifference;
+            }
+        }
+            
+    }
+
+    friendsData.push(userData);
+    
+    res.json(perfectMatch)
+
+    });
+}
